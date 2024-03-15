a1241192e7947abccd618ed55dbd2cb0d804b378
Josh Dowrick <j.dowrick@tees.ac.uk>
2024-03-15 14:07:46 +0000

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "GitKraken1"
+include(":app")
+ 
